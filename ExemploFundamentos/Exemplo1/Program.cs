18453a59b47c0236e60c52d71f18039f90bc9605
William Silva <william.silva@viannasempre.com.br>
2024-08-05 15:08:51 -0300

    Realizando conversões de maneira segura

--- ExemploFundamentos/Exemplo1/Program.cs
+++ ExemploFundamentos/Exemplo1/Program.cs
@@ -6,6 +6,24 @@ class Program
 {
     public static void Main(string[] args)
     {
+        // Tipos de dados.
+        string apresentacao = "Olá, seja bem vindo!";
+        int quantidade = 1;
+        double altura = 1.83D;
+        decimal preco = 1.80M;
+        bool condicao = true;
+        
+        // Trabalhando com o tipo de data e hora.
+        DateTime dataAtual = DateTime.Now;
+        Console.WriteLine($"Data Hora atual: {dataAtual:dd/MM/yyyy HH:mm}");
+
+        Console.WriteLine(apresentacao);
+        Console.WriteLine($"Valores das variável quantidade: {quantidade}");
+        Console.WriteLine($"Valores das variável altura: {altura.ToString("0.00")}");
+        Console.WriteLine($"Valores das variável preco: {preco:C}");
+        Console.WriteLine($"Valores das variável condicao: {condicao}");
+
+        // Instanciação de um objeto do tipo Pessoa.
         Pessoa p = new Pessoa("William", 27);
         p.Apresentar();
     }
---
+++ ExemploFundamentos/Exemplo2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exemplo2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Convertendo de maneira segura.
+        string a = "15-";
+
+        // A variável de saída pode ser declarada previamente ou omitida e declarada diretamente no corpo do TryParse.
+        // int b = 0; 
+
+        int.TryParse(a , out int b);
+
+        Console.WriteLine(b);
+        Console.WriteLine("Conversão realizada com sucesso!");
+    }
+}
